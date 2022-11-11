5c2019d31459b2f7a0369fe00c498b55cdd41883
Ernest Hong <hsh3592@gmail.com>
2022-11-11 20:52:57 +0900

    Use alamofire

--- SendbirdAppCenterSample/AppDelegate.swift
+++ SendbirdAppCenterSample/AppDelegate.swift
@@ -6,27 +6,30 @@
 //
 
 import UIKit
-import SendbirdChatSDK
+//import SendbirdChatSDK
+import Alamofire
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let initParams = InitParams(
-            applicationId: "APP_ID",
-            isLocalCachingEnabled: true,
-            logLevel: .info
-        )
         
-        SendbirdChat.initialize(
-            params: initParams,
-            migrationStartHandler: {
-                // Migration starts.
-            },
-            completionHandler: { error in
-                // Migration completed.
-            }
-        )
+        _ = AlamofireNotifications.self
+//        let initParams = InitParams(
+//            applicationId: "APP_ID",
+//            isLocalCachingEnabled: true,
+//            logLevel: .info
+//        )
+//
+//        SendbirdChat.initialize(
+//            params: initParams,
+//            migrationStartHandler: {
+//                // Migration starts.
+//            },
+//            completionHandler: { error in
+//                // Migration completed.
+//            }
+//        )
         
         return true
     }
